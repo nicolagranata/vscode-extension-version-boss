6de61a7fc041d45e25fb63d3c9196834d898a1b3
nicolagranata <49106793+nicolagranata@users.noreply.github.com>
2019-10-31 19:05:08 +0100

    Commit from VSCode Extensions Manager v. 1.0.23 @ 2019-10-31 18:04:32

--- _master/test.php
+++ _master/test.php
@@ -1,3 +1,30 @@
 <?php
 
+/*
+<!#CR>
+************************************************************************************************************************
+*                                                    Copyrigths ©                                                      *
+* -------------------------------------------------------------------------------------------------------------------- *
+*          Authors Names    > Nicola Granata                                                                           *
+*          Authors Email    > info@daemoncms.com                                                                       *
+*          Company Name     > Active Sahde di Nicola Granata                                                           *
+*          Company Websites > http://www.daemoncms.com                                                                 *
+* -------------------------------------------------------------------------------------------------------------------- *
+*                                           File and License Informations                                              *
+* -------------------------------------------------------------------------------------------------------------------- *
+*          File Name        > <!#FN> test.php </#FN>                                                                   
+*          File Birth       > <!#FB> 2019/10/31 18:11:00.904 </#FB>                                                    *
+*          File Mod         > <!#FT> 2019/10/31 19:04:20.017 </#FT>                                                    *
+*          License          > <!#LT> BSD-3-Clause-Attribution </#LT>                                                   
+*                             <!#LU> https://spdx.org/licenses/BSD-3-Clause-Attribution.html </#LU>                    
+*                             <!#LD> This file may not be redistributed in whole or significant part. </#LD>           
+*          File Version     > <!#FV> 0.0.0 </#FV>                                                                      
+*                                                                                                                      *
+******************************************* VSCode Extension: Version Boss *********************************************
+</#CR>
+*/
+
+
+
+
 ?>
